a3c97bd2584d2e969be0a52ef1a30a13be05b43b
Felix Roske <felix.roske@zalando.de>
2017-02-17 15:52:01 +0100

    Upgrade postgres

--- src/main/java/de/zalando/playground/DataSourceConfig.java
+++ src/main/java/de/zalando/playground/DataSourceConfig.java
@@ -2,18 +2,20 @@ package de.zalando.playground;
 
 import java.io.IOException;
 
+import javax.persistence.Embedded;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.opentable.db.postgres.embedded.EmbeddedPostgreSQL;
+import com.opentable.db.postgres.embedded.EmbeddedPostgres;
+
 
 @Configuration
 public class DataSourceConfig {
 
     @Bean
     public DataSource dataSource() throws IOException {
-        return EmbeddedPostgreSQL.start().getPostgresDatabase();
+        return EmbeddedPostgres.start().getPostgresDatabase();
     }
 }
